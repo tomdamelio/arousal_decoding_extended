b924fae6331b982837f7670736d373362fcd4562
tomdamelio <tomasdamelio@psi.uba.ar>
2022-08-01 23:07:12 -0300

    modify add_annotations.py file: ready to run on server

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # arousal_decoding_extended
-`convert_DEAP_to_bids_server.py`: file necessary to convert DEAP .bdf foles into bids standard.
-`convert_DEAP_to_bids_local.py`: idem to `convert_DEAP_to_bids_server.py` but runs locally.
-`convert_deap_to_bids_without_function`: DEPRECATED. Idem to `convert_DEAP_to_bids_local.py` but runs without functions, parser, ...
 
+`convert_DEAP_to_bids_server.py`: file necessary to convert DEAP .bdf files into bids standard. Runs also locally on my PC.
+`convert_DEAP_to_bids_local.py`: idem to `convert_DEAP_to_bids_server.py` but runs locally.
+`convert_deap_to_bids_without_function`: DEPRECATED. Idem to ut runs without functions, parser, ...
+`convert_DEAP_to_bids_local.py`: DEPRECATED. I can also run server file locally.
+`add_annotations.py`: create two files.
+1. raw files + annotations (saved in `./outputs/DEAP-bids`)
+2. annotations files (saved in `./outputs/annotations_bad_no_stim`)
+`check_len_stimuli_presentation.py`: check how many trials are detected after running `add_annotations.py`
---
+++ check_len_stimuli_presentation.py
@@ -0,0 +1,107 @@
+# By running this script, we could check how many stimuli
+# are presented in every subject.
+
+# As a result we can see that all subjects have 40 trials (as expected by experimental design).
+# But subject 28 which exceptionally has 37 trials.
+
+
+import os.path as op
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import mne
+from mne_bids import BIDSPath
+from sklearn.model_selection import (
+     cross_val_score, ShuffleSplit, KFold)
+from sklearn.preprocessing import StandardScaler
+from sklearn import preprocessing
+from sklearn.pipeline import make_pipeline
+from sklearn.decomposition import PCA
+from sklearn.linear_model import RidgeCV
+#from meegpowreg import make_filter_bank_regressor, make_filter_bank_transformer
+from subject_number import subject_number as subjects
+from numpy.lib.npyio import save
+import json
+from collections import defaultdict
+import pathlib
+
+DEBUG = False
+
+if DEBUG:
+    N_JOBS = 1
+    subjects = subjects[:1]
+
+if os.name == 'nt':
+    derivative_path = 'C:/Users/dadam/arousal_decoding_extended/outputs/DEAP-bids'  
+    annotations_root = pathlib.Path(
+        "C:/Users/dadam/arousal_decoding_extended/outputs/annotations_bad_no_stim").expanduser()
+else:
+    derivative_path = 'storage/store/data/DEAP/outputs/DEAP-bids/derivatives/mne-bids-pipeline'
+
+all_subs_epochs_selected = dict()
+
+for subject in subjects:
+    
+    raw_path = BIDSPath(subject= subject, 
+                           task='rest',
+                           datatype='eeg',
+                           root=derivative_path,
+                           extension='.fif',
+                           check=False)
+    # Read EDA data
+    raw = mne.io.read_raw_fif(raw_path)
+    
+    # Pick only EDA data
+    raw_eda = raw.copy().pick_channels(['EDA'])
+    
+    # Add annotations (bad_no_stim) to data
+    fname_annot = op.join(annotations_root,'sub-'+ subject + '_annot.fif')
+    annot_from_file = mne.read_annotations(fname=fname_annot, sfreq=raw.info['sfreq'])
+    raw.set_annotations(annot_from_file)
+    
+    events = mne.make_fixed_length_events(
+        raw, id=3000, start=0,
+        duration=5.,
+        overlap=0.)
+    event_id = dict(rest=3000)
+    
+    metadata, _, _ = mne.epochs.make_metadata(
+        events=events, event_id=event_id,
+        tmin=0., tmax =5.,
+        sfreq=raw.info['sfreq'])
+    
+    epochs = mne.Epochs(raw, events=events, event_id=event_id,
+                    metadata=metadata, reject_by_annotation=True,
+                    baseline=None)
+    
+    epochs.drop_bad()
+
+    epochs_selected = epochs.selection
+    stim_dict =  defaultdict(list)
+    music_stim = 1 
+
+    for epoch_idx in epochs_selected:
+        if str(music_stim) not in stim_dict:
+            stim_dict[str(music_stim)].append(epoch_idx)        
+        elif len(stim_dict.get(str(music_stim))) == 1:
+            stim_dict[str(music_stim)].append(epoch_idx)
+        elif (epoch_idx-stim_dict.get(str(music_stim))[0]) <= 12:
+            stim_dict[str(music_stim)].append(epoch_idx)    
+        else:
+            music_stim += 1
+            
+    all_subs_epochs_selected[str(subject)] = stim_dict
+        
+# %%
+for subject in subjects:
+    if len(all_subs_epochs_selected[str(subject)]) != 40:
+        print('sub',subject, '->' ,len(all_subs_epochs_selected[str(subject)]))
+        
+#%%
+for subject in subjects:
+    print('sub',subject, '->' ,len(all_subs_epochs_selected[str(subject)]))
+        
+
+# Insepct a specific subjet
+# all_subs_epochs_selected['09']
